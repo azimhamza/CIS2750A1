95bec601c6420cd423601afd499703860fae25fd
Azim Hamza <aazismail@gmail.com>
2023-01-29 14:44:33 -0500

    Fixed Bugs, Added Bond_PTR

--- mol.c
+++ mol.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "mol.h"
 #include <math.h>
+#include <string.h>
 
 // setter for an atom
 void atomset(atom *atom, char element[3], double *x, double *y, double *z)
@@ -37,9 +38,9 @@ void bondset(bond *bond, atom *a1, atom *a2, unsigned char epairs)
 void bondget(bond *bond, atom **a1, atom **a2, unsigned char *epairs)
 {
     // getting the bonds a1 and a2
-    a1 = bond->a1;
-    a2 = bond->a2;
-    epairs = bond->epairs;
+    *a1 = bond->a1;
+    *a2 = bond->a2;
+    *epairs = bond->epairs;
 };
 
 // creating space
@@ -71,6 +72,27 @@ molecule *molmalloc(unsigned short atom_max, unsigned short bond_max)
     if (mole->atom_ptrs == NULL)
     {
         printf("<Memory Error> for Atom Pointer (allocating)\n");
+        exit(1);
+    }
+
+    // set atom max
+    mole->bond_max = bond_max;
+    // initate array
+    mole->bond_no = 0;
+    // mallocing the bond, multiplying the total bond_max with the size of bond
+    mole->bonds = (bond *)malloc(bond_max * sizeof(bond));
+    // checks if the bonds are null
+    if (mole->bonds == NULL)
+    {
+        printf("<Memory Error> for Bond Array (allocating)\n");
+        exit(1);
+    }
+    // allocating pointer memory
+    mole->bond_ptrs = (bond **)malloc(bond_max * sizeof(bond *));
+    // null checker for bond_ptr
+    if (mole->bond_ptrs == NULL)
+    {
+        printf("<Memory Error> for Bond Pointer (allocating)\n");
         exit(1);
     }
     return mole;
@@ -147,9 +169,11 @@ void molfree(molecule *ptr)
 // adds it to the end
 void molappend_atom(molecule *molecule, atom *atom)
 {
-   int i;
-    for (i = 0; i < molecule->atom_no; i++) {
-        if (molecule->atom_ptrs[i] == NULL) {
+    int i;
+    for (i = 0; i < molecule->atom_no; i++)
+    {
+        if (molecule->atom_ptrs[i] == NULL)
+        {
             molecule->atoms[i] = *atom;
             break;
         }
@@ -157,15 +181,20 @@ void molappend_atom(molecule *molecule, atom *atom)
     molecule->atom_ptrs[molecule->atom_no] = &molecule->atoms[i];
     molecule->atom_no++;
 
-    if (molecule->atom_no == molecule->atom_max) {
-        if (molecule->atom_max == 0) {
+    if (molecule->atom_no == molecule->atom_max)
+    {
+        if (molecule->atom_max == 0)
+        {
             molecule->atom_max = 1;
-        } else {
+        }
+        else
+        {
             molecule->atom_max *= 2;
         }
         molecule->atoms = realloc(molecule->atoms, sizeof(atom) * molecule->atom_max);
         molecule->atom_ptrs = realloc(molecule->atom_ptrs, sizeof(*atom) * molecule->atom_max);
-        for (i = 0; i < molecule->atom_no; i++) {
+        for (i = 0; i < molecule->atom_no; i++)
+        {
             molecule->atom_ptrs[i] = &molecule->atoms[i];
         }
     }
@@ -173,9 +202,11 @@ void molappend_atom(molecule *molecule, atom *atom)
 
 void molappend_bond(molecule *molecule, bond *bond)
 {
-     int i;
-    for (i = 0; i < molecule->bond_no; i++) {
-        if (molecule->bond_ptrs[i] == NULL) {
+    int i;
+    for (i = 0; i < molecule->bond_no; i++)
+    {
+        if (molecule->bond_ptrs[i] == NULL)
+        {
             molecule->bonds[i] = *bond;
             break;
         }
@@ -183,51 +214,54 @@ void molappend_bond(molecule *molecule, bond *bond)
     molecule->bond_ptrs[molecule->bond_no] = &molecule->bonds[i];
     molecule->bond_no++;
 
-    if (molecule->bond_no == molecule->bond_max) {
-        if (molecule->bond_max == 0) {
+    if (molecule->bond_no == molecule->bond_max)
+    {
+        if (molecule->bond_max == 0)
+        {
             molecule->bond_max = 1;
-        } else {
+        }
+        else
+        {
             molecule->bond_max *= 2;
         }
         molecule->bonds = realloc(molecule->bonds, sizeof(bond) * molecule->bond_max);
         molecule->bond_ptrs = realloc(molecule->bond_ptrs, sizeof(*bond) * molecule->bond_max);
-        for (i = 0; i < molecule->bond_no; i++) {
+        for (i = 0; i < molecule->bond_no; i++)
+        {
             molecule->bond_ptrs[i] = &molecule->bonds[i];
         }
     }
 }
 
+int cmp(const void *a, const void *b)
+{
+    double a_z = (*(atom **)a)->z;
+    double b_z = (*(atom **)b)->z;
+    if (a_z > b_z)
+        return 1;
+    else if (a_z < b_z)
+        return -1;
+    else
+        return 0;
+}
 
-  int cmp(const void *a, const void *b)
-    {
-        double a_z = (*(atom **)a)->z;
-        double b_z = (*(atom **)b)->z;
-        if (a_z > b_z)
-            return 1;
-        else if (a_z < b_z)
-            return -1;
-        else
-            return 0;
-    }
-
-     int bondcmp(const void *a, const void *b)
-    {
-        double a_z = ((*(bond **)a)->a1->z + (*(bond **)a)->a2->z) / 2;
-        double b_z = ((*(bond **)b)->a1->z + (*(bond **)b)->a2->z) / 2;
-        if (a_z > b_z)
-            return 1;
-        else if (a_z < b_z)
-            return -1;
-        else
-            return 0;
-    }
+int bondcmp(const void *a, const void *b)
+{
+    double a_z = ((*(bond **)a)->a1->z + (*(bond **)a)->a2->z) / 2;
+    double b_z = ((*(bond **)b)->a1->z + (*(bond **)b)->a2->z) / 2;
+    if (a_z > b_z)
+        return 1;
+    else if (a_z < b_z)
+        return -1;
+    else
+        return 0;
+}
 
 void molsort(molecule *molecule)
 {
-  
+
     qsort(molecule->atom_ptrs, molecule->atom_no, sizeof(atom *), cmp);
 
-   
     qsort(molecule->bond_ptrs, molecule->bond_no, sizeof(bond *), bondcmp);
 }
 
@@ -255,7 +289,7 @@ void yrotation(xform_matrix xform_matrix, unsigned short deg)
     xform_matrix[0][0] = cos(rad);
     xform_matrix[0][1] = 0;
     xform_matrix[0][2] = sin(rad);
-    xform_matrix[1][0] = 0; 
+    xform_matrix[1][0] = 0;
     xform_matrix[1][1] = 1;
     xform_matrix[1][2] = 0;
     xform_matrix[2][0] = -sin(rad);
@@ -263,8 +297,8 @@ void yrotation(xform_matrix xform_matrix, unsigned short deg)
     xform_matrix[2][2] = cos(rad);
 }
 
-
-void computeRotationMatrix(xform_matrix xform_matrix, double rad) {
+void computeRotationMatrix(xform_matrix xform_matrix, double rad)
+{
     xform_matrix[0][0] = cos(rad);
     xform_matrix[0][1] = -sin(rad);
     xform_matrix[0][2] = 0;
@@ -276,15 +310,16 @@ void computeRotationMatrix(xform_matrix xform_matrix, double rad) {
     xform_matrix[2][2] = 1;
 }
 
-void zrotation(xform_matrix xform_matrix, unsigned short deg) {
+void zrotation(xform_matrix xform_matrix, unsigned short deg)
+{
     // Convert deg to radians
     double rad = deg * (M_PI / 180);
     // Compute the transformation matrix
     computeRotationMatrix(xform_matrix, rad);
 }
 
-
-void transform_atom(atom *a, xform_matrix matrix) {
+void transform_atom(atom *a, xform_matrix matrix)
+{
     double x = a->x;
     double y = a->y;
     double z = a->z;
@@ -294,8 +329,10 @@ void transform_atom(atom *a, xform_matrix matrix) {
     a->z = matrix[2][0] * x + matrix[2][1] * y + matrix[2][2] * z;
 }
 
-void mol_xform(molecule *molecule, xform_matrix matrix) {
-    for (int i = 0; i < molecule->atom_no; i++) {
+void mol_xform(molecule *molecule, xform_matrix matrix)
+{
+    for (int i = 0; i < molecule->atom_no; i++)
+    {
         transform_atom(molecule->atom_ptrs[i], matrix);
     }
 }
