97b1bdf2f9f150edcf6455c5d3a5c8d5f8426063
Azim Hamza <aazismail@gmail.com>
2023-01-31 02:18:56 -0500

    headerfile updated

--- mol.h
+++ mol.h
@@ -27,19 +27,20 @@ typedef struct molecule {
 typedef double xform_matrix[3][3];
 
 
-void atomset(atom *atom, char element[3], double *x, double *y, double *z);
-void atomget(atom *atom, char element[3], double *x, double *y, double *z);
-void bondset(bond *bond, atom *a1, atom *a2, unsigned char epairs);
-void bondget(bond *bond, atom **a1, atom **a2, unsigned char *epairs);
-molecule *molmalloc(unsigned short atom_max, unsigned short bond_max);
-molecule *molcopy(molecule *src);
-void molfree(molecule *ptr);
-void molappend_atom(molecule *molecule, atom *atom);
-void molappend_bond(molecule *molecule, bond *bond);
-int compare_atom_z(const void *a, const void *b);
-int compare_bond_z(const void *a, const void *b);
-void molsort(molecule *molecule);
-void xrotation(xform_matrix xform_matrix, unsigned short deg);
-void yrotation(xform_matrix xform_matrix, unsigned short deg);
-void zrotation(xform_matrix xform_matrix, unsigned short deg);
+void atomset(atom *atom, char element[3], double *x, double *y, double *z)
+void atomget(atom *atom, char element[3], double *x, double *y, double *z)
+void bondset(bond *bond, atom *a1, atom *a2, unsigned char epairs)
+void bondget(bond *bond, atom **a1, atom **a2, unsigned char *epairs)
+molecule *molmalloc(unsigned short atom_max, unsigned short bond_max)
+molecule *molcopy(molecule *src)
+void molfree(molecule *ptr)
+void molappend_atom(molecule *molecule, atom *at)
+void molappend_bond(molecule *molecule, bond *bo)
+void molsort(molecule *molecule)
+int compare_atom_z(const void *a, const void *b)
+int compare_bond_z(const void *a, const void *b)
+void xrotation(xform_matrix xform_matrix, unsigned short deg)
+void yrotation(xform_matrix xform_matrix, unsigned short deg)
+void zrotation(xform_matrix xform_matrix, unsigned short deg)
+void mol_xform(molecule *molecule, xform_matrix matrix)
 
